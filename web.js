2c61a175eed07d4b813c76dcb88033f7bdb19212
Ubuntu <ubuntu@ip-172-31-28-208.us-west-2.compute.internal>
2013-07-15 04:17:12 +0000

    fix type-o

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
   var hello = new Buffer(fs.readFileSync('index.html'));
-  response.send(hello.toString('utf-8');
+  response.send(hello.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
