e19fe1f1af08ae30e048bec99efd88b3cdcfa4c6
Ubuntu <ubuntu@ip-172-31-28-208.us-west-2.compute.internal>
2013-07-15 04:12:48 +0000

    output index.html file with fs.readFileSync and Buffer.toString

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  var hello = new Buffer(fs.readFileSync('index.html'));
+  response.send(hello.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
